aa54ad47207b68f138a35108c7e63bdd763fcb50
abu sayeed <sayeed0209@gmail.com>
2021-05-21 10:40:19 +0200

    abstacting class

--- classes_and_arrow_functions/app.js
+++ classes_and_arrow_functions/app.js
@@ -1,34 +1,49 @@
-// exercise 1
-(() => {
-	const a = 5;
-	const b = 6;
-	const result = a + b;
-	console.log(result);
-})();
-// exercise 1
-((a, b) => console.log(a + b))(5, 9);
+// nivel 1 Ejercicio 1
+((a, b) => {
+	console.log(a + b);
+})(2, 99);
 
-// exercise 2
+// nivel 2 Ejercicio 1
 const annonymus = name => {
 	return { firstName: name };
 };
-const name = annonymus('sayeed');
+const name = annonymus("sayeed");
 console.log(name);
 
-// exercise 3
+// nivel 2 Ejercicio 2
 
-class Person {
-	constructor(name) {
-		this.name = name;
+class Persona {
+	constructor(nombre = "sayeed") {
+		this.nombre = nombre;
 	}
 
-	sayName() {
-		return `My name is ${this.name}`;
+	decirNombre() {
+		console.log(`Mi nombre es ${this.nombre}`);
 	}
 }
 
-const p = new Person('Robin');
-console.log(p.sayName());
+const persona = new Persona();
+const persona1 = new Persona("Robin");
+console.log(persona.decirNombre());
+console.log(persona1.decirNombre());
 
-// exercise 4
-// Create an object-creating function, abstracting the definition of the classes. Invoke it with different definitions
+// nivel 3 Ejercicio 1
+// // Create an object-creating function, abstracting the definition of the classes. Invoke it with different definitions
+const Color = function () {
+	this.name = "red";
+	throw new Error("Can not create an instance of an abstract class");
+};
+
+Color.prototype.newColor = () => {
+	return "Color name is" + this.name;
+};
+// const color = new Color();
+const drawColor = function (color) {
+	this.color = color;
+};
+
+drawColor.prototype = Object.create(Color.prototype);
+const draw = new drawColor("green");
+console.log(draw);
+console.log(draw instanceof drawColor);
+console.log(draw instanceof Color);
