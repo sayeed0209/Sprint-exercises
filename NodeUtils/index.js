57b1dfbf8f61195ab9e5e1455d418f0880eb48c1
abu sayeed <sayeed0209@gmail.com>
2021-05-26 13:59:30 +0200

    nivel 3

--- NodeUtils/index.js
+++ NodeUtils/index.js
@@ -36,3 +36,14 @@ pipeline(source, gzip, destination, err => {
 		process.exitCode = 1;
 	}
 });
+
+//nivell 3
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+async function lsExample() {
+	const { stdout, stderr } = await exec("ls");
+	console.log("stdout:", stdout);
+	console.error("stderr:", stderr);
+}
+lsExample();
