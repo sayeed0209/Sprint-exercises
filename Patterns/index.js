326c56fa892c60c7877301d0fae3b2c205cc7aa6
abu sayeed <sayeed0209@gmail.com>
2021-06-28 11:35:43 +0200

    refactor callbackhell

--- Patterns/index.js
+++ Patterns/index.js
@@ -1,27 +1,29 @@
 // exe 1
-const fs = require("fs").promises;
-const path = require("path");
-const inbox = path.join(__dirname, "inbox");
-const outbox = path.join(__dirname, "outbox");
+const { readdir, readFile, writeFile } = require("fs");
+const { join } = require("path");
+const inbox = join(__dirname, "inbox");
+const outbox = join(__dirname, "outbox");
 const reverseText = str => str.split("").reverse().join("");
 
 // Read and reverse contents of text files in a directory
-async function readAndWriteReverse(dir1, dir2) {
-	try {
-		const files = await fs.readdir(dir1);
-		for (let file of files) {
-			const readfile = await fs.readFile(path.join(dir1, file), "utf8");
-			const write = await fs.writeFile(
-				path.join(dir2, file),
-				reverseText(readfile),
-				"utf8"
-			);
-		}
-	} catch (err) {
-		console.log(err.message);
-	}
+function dir(err, files) {
+	if (err) return console.log("Error:Folder inaccessible");
+	files.forEach(file => readFileFromDir(file));
 }
-readAndWriteReverse(inbox, outbox);
+function readFileFromDir(file) {
+	readFile(join(inbox, file), "utf8", invertedFile);
+}
+
+function invertedFile(err, data) {
+	if (err) return console.log("Error: File error ");
+	writeFile(join(outbox, file), reverseText(data), errorFunc);
+}
+
+function errorFunc(err) {
+	if (err) return console.log("Error:File could not be saved!");
+	console.log(`${this.file} was saved successfully`);
+}
+readdir(inbox, dir);
 // exe 2
 
 const ScoreBoard = require("./ScoreBoard");
