4cc8766cb00081b0c8a424d31f2d26a08fecfc88
abu sayeed <sayeed0209@gmail.com>
2021-07-28 16:24:05 +0200

    refactor promise and & callback functionalities

--- promise_callbacks/app.js
+++ promise_callbacks/app.js
@@ -1,43 +1,43 @@
 // exercise 1
 // Create a function that returns a Promise that invokes the resolve () function or reject () that it receives. Invoke it from the outside by passing both functions that print a different message in each case.
-// const randomFunc = str => {
-// 	return new Promise((resolve, reject) => {
-// 		if (str) {
-// 			resolve(str);
-// 		} else {
-// 			reject("Error: Failed to resolve promise");
-// 		}
-// 	});
-// };
+const randomFunc = str => {
+	return new Promise((resolve, reject) => {
+		if (str) {
+			resolve(str);
+		} else {
+			reject("Error: Failed to resolve promise");
+		}
+	});
+};
 
-// // reslove promise
-// randomFunc("Hello Sayeed")
-// 	.then(msg => {
-// 		console.log(msg);
-// 	})
-// 	.catch(err => {
-// 		console.log(err);
-// 	});
+// reslove promise
+randomFunc("Hello Sayeed")
+	.then(msg => {
+		console.log(msg);
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
-// // // reject promise
-// randomFunc()
-// 	.then(msg => {
-// 		console.log(msg);
-// 	})
-// 	.catch(err => {
-// 		console.log(err);
-// 	});
+// // reject promise
+randomFunc()
+	.then(msg => {
+		console.log(msg);
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
-// // exercise 2
-// const greetings = (msg, callback) => {
-// 	callback(msg);
-// };
-// const msg = name =>
-// 	name == "sayeed"
-// 		? console.log("hello " + name)
-// 		: console.log("bye bye " + name);
-// console.log(greetings("sayeed", msg));
-// // exercise 3
+// exercise 2
+const greetings = (msg, callback) => {
+	callback(msg);
+};
+const msg = name =>
+	name == "sayeed"
+		? console.log("hello " + name)
+		: console.log("bye bye " + name);
+console.log(greetings("sayeed", msg));
+// exercise 3
 let employees = [
 	{
 		id: 1,
